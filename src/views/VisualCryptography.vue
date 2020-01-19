b17bde2d403309843eeb90a8f0cf93228692c5fd
Wojciech Kasperski <wojciech.kasperski97@gmail.com>
2020-01-19 12:00:11 +0100

    Visual Cryptography Description

--- src/views/VisualCryptography.vue
+++ src/views/VisualCryptography.vue
@@ -1,7 +1,21 @@
 <template>
   <v-container>
-    <VisualEncryptTwoOne />
-    <VisualEncryptTwoTwo />
+    <v-row>
+      <v-col cols="12">
+        <h1>Visual Cryptography</h1>
+      </v-col>
+      <VisualEncryptTwoOne />
+      <VisualEncryptTwoTwo />
+      <v-col cols="12">
+        <h2>How to use?</h2>
+        <p>
+          Just click on left side of picture and make some image, then cipher
+          it. You can also save image and overlay cipher pictures. By using
+          <code>?c=40&r=40</code> in application URL you can specify numer of
+          columns and rows.
+        </p>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
